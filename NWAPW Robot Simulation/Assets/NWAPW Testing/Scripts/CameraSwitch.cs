41acc12c9d5f6968dbb2173b2d26f19afed5f9bd
yanjinhai <jinhaiyan777@gmail.com>
2019-07-16 14:06:40 -0700

    Merging camera step 1

---
+++ NWAPW Robot Simulation/Assets/NWAPW Testing/Scripts/CameraSwitch.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSwitch : MonoBehaviour
+{
+    public GameObject FirstPerson;
+    public GameObject ThirdPerson;
+    public GameObject TopView;
+    public int camMode;
+    public int test;
+    // Start is called before the first frame update
+    void Start()
+    {
+        camMode = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switchCamera();
+    
+    }
+    void switchCamera() {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            camMode++;
+            if (camMode > 2) {
+                camMode = 0;
+            }
+            if (camMode == 0)
+            {
+   
+                TopView.SetActive(true);
+                ThirdPerson.SetActive(false);
+                FirstPerson.SetActive(false);
+            }
+            else if (camMode == 1)
+            {
+                FirstPerson.SetActive(false);
+                ThirdPerson.SetActive(true);
+                TopView.SetActive(false);
+            }
+            else if (camMode == 2)
+            {
+                FirstPerson.SetActive(true);
+                ThirdPerson.SetActive(false);
+                TopView.SetActive(false);
+
+            }
+        }
+    }
+}
--- NWAPW Robot Simulation/Assets/NWAPW Testing/Scripts/PlayerMovement.cs
+++ NWAPW Robot Simulation/Assets/NWAPW Testing/Scripts/PlayerMovement.cs
@@ -4,22 +4,40 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public float speed = 1;
     // Start is called before the first frame update
-    private Rigidbody rb;
+   // private Rigidbody rb;
 
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+
+        //rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        /*
         float h = Input.GetAxis("Horizontal") *5;
         float v = Input.GetAxis("Vertical") * 5;
         Vector3 vel = rb.velocity;
         vel.x = h;
         vel.z = v;
-        rb.velocity = vel;
+        rb.velocity = vel;*/
+        if (Input.GetKey("right"))
+        {
+            transform.Rotate(0, 1.5f, 0);
+        }
+        else if (Input.GetKey("left")) {
+            transform.Rotate(0,-1.5f, 0);
+        }
+        if (Input.GetKey("up"))
+        {
+            transform.position += transform.forward * 0.3f;
+        }
+        else if (Input.GetKey("down")) {
+            transform.position -= transform.forward * 0.3f;
+
+        }
     }
 }
