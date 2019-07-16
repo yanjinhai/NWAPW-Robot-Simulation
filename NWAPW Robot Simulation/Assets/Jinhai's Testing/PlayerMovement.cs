6771f00f4c623e69fb2574de5b9bf9dbd099bbe6
JasonZhanggg <383349@bsd48.org>
2019-07-16 10:18:18 -0700

    Block move with arrow keys

---
+++ NWAPW Robot Simulation/Assets/Jinhai's Testing/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal") *5;
+        float v = Input.GetAxis("Vertical") * 5;
+        Vector3 vel = rb.velocity;
+        vel.x = h;
+        vel.z = v;
+        rb.velocity = vel;
+    }
+}
