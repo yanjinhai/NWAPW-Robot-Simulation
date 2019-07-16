c019396efd774211ae9f2f911aaa75020689c089
yanjinhai <jinhaiyan777@gmail.com>
2019-07-16 13:14:00 -0700

    fixed an error

--- NWAPW Robot Simulation/Assets/NWAPW Testing/Scripts/RobotMovement.cs
+++ NWAPW Robot Simulation/Assets/NWAPW Testing/Scripts/RobotMovement.cs
@@ -10,7 +10,7 @@ public class RobotMovement : MonoBehaviour
     public float rotateSpeed = 20.0f;
     public float moveSpeed = 2.0f;
     int rotateDir = 1;// clockwise/counterclockwise
-    bool goGo;
+    public bool goGo;
 
     void Start()
     {
