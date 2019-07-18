60e341c36a4b7b998775ed068548f984026a339b
Robert Young <ryoungster@users.noreply.github.com>
2019-07-18 11:12:53 -0700

    NavPoint added

---
+++ NWAPW Robot Simulation/Assets/NWAPW Testing/Scripts/NavPoint.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NavPoint : MonoBehaviour
+{
+    public Vector3 point;
+    public float gCost;
+    public GameObject from;
+    public float fCost;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        point = this.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
