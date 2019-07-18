4c70f37444066cd912b2fecb262794a85de82399
Robert Young <ryoungster@users.noreply.github.com>
2019-07-18 09:32:09 -0700

    Privatised Grabbables

--- NWAPW Robot Simulation/Assets/NWAPW Testing/Scripts/GrabRelease.cs
+++ NWAPW Robot Simulation/Assets/NWAPW Testing/Scripts/GrabRelease.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class GrabRelease : MonoBehaviour
 {
     public GameObject grabbedObj;
-    public GameObject[] grabbableObjs;
+    private GameObject[] grabbableObjs;
     public GameObject releasedObjs;
     
     Vector3 offset = new Vector3(0, 0.1f, 1.05f);
