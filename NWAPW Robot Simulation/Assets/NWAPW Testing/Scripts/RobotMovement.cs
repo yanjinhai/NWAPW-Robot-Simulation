1ce52ae7eccbbd8932cc3836bcb158697feeb001
yanjinhai <jinhaiyan777@gmail.com>
2019-07-26 13:59:44 -0700

    minor text formatting

--- NWAPW Robot Simulation/Assets/NWAPW Testing/Scripts/RobotMovement.cs
+++ NWAPW Robot Simulation/Assets/NWAPW Testing/Scripts/RobotMovement.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class RobotMovement : MonoBehaviour
 {
-
     // Variables, some with default values
     Vector3 targetPos;
     Vector3 relativePos;
@@ -19,23 +18,19 @@ public class RobotMovement : MonoBehaviour
 
     void Start()
     {
-
         // Robot set to be "moving" intialy to properly go through if statements
         isMoving = true;
     }
 
     void Update()
     {
-
         // Runs if the robot is set to auto and has not reached its target
         if (isMoving && run)
         {
-
             // Checks whether the robot is within the deadband this varies by target
             relativePos = targetPos - this.transform.position;
             if (relativePos.magnitude <= positionDeadband)
             {
-
                 // Sets the robot to not moving and ends the update
                 isMoving = false;
                 return;
@@ -44,13 +39,11 @@ public class RobotMovement : MonoBehaviour
             // If backwards movement is not enabled
             if (!moveBack)
             {
-
                 // Checks if the robot is pointing at the target
                 float relativeAngle = Vector3.SignedAngle(relativePos, this.transform.forward, this.transform.up);
                 float relativeRotationDir = relativeAngle / (Mathf.Abs(relativeAngle));
                 if (Mathf.Abs(relativeAngle) > 1)
                 {
-
                     // Rotates the robot towards the target
                     this.transform.Rotate(0, rotateSpeed * Time.deltaTime * relativeRotationDir * -1, 0);
                 }
@@ -58,7 +51,6 @@ public class RobotMovement : MonoBehaviour
                 // Else the robot is pointing at the target
                 else
                 {
-
                     // Moves the robot forward, toward the target
                     this.transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
                 }
