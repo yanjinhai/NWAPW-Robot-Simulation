f807963f69244feff676ba4df9e9dc3f136972a6
Robert Young <ryoungster@users.noreply.github.com>
2019-07-29 10:26:39 -0700

    NavPoint

--- NWAPW Robot Simulation/Assets/NWAPW Testing/Scripts/NavPoint.cs
+++ NWAPW Robot Simulation/Assets/NWAPW Testing/Scripts/NavPoint.cs
@@ -4,28 +4,43 @@ using UnityEngine;
 
 public class NavPoint : MonoBehaviour
 {
+
+    // Variables, some with default values
     public Vector3 point;
     public float gCost = Mathf.Infinity;
     public NavPoint from;
     public float fCost = Mathf.Infinity;
+    public float deadBand = -0.5f;
 
+    // Awake is used here to prempt intial pathfinding run on start
     void Awake()
     {
-        point = new Vector3 (this.transform.position.x,.5f, this.transform.position.z);
-        from = this;
+        // Set up point at its location and resets values to defaults
+        point = new Vector3(this.transform.position.x, .5f, this.transform.position.z);
+        ResetValues();
+
+        // Sets deadband for objects the robot runs into
+        if (this.gameObject.tag == "CollectableObject")
+        {
+            deadBand = this.gameObject.GetComponent<Collider>().bounds.size.x / 2;
+        }
     }
 
     void Update()
     {
+        // Updates point for moving objects
         point = new Vector3(this.transform.position.x, .5f, this.transform.position.z);
     }
 
-    public void ResetValues() {
+    // Resets from and costs to default values, except player, due to it having special values
+    public void ResetValues()
+    {
         if (this.gameObject.tag != "Player")
         {
             gCost = Mathf.Infinity;
             fCost = Mathf.Infinity;
-            from = this;
+
         }
+        from = this;
     }
 }
